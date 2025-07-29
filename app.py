3164f47aa6c0b640c05c3acaa13998c79cf90d9c
Ahmad Shareef <ahmadbinshareef@gmail.com>
2025-07-29 15:35:38 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
- def greet(name):
-        return f"Hello, {name}!"
+def greet(name):
+    return f"Hello, {name}!"
 
-
-if__name__=="__main__":
+if __name__ == "__main__":
     print(greet("Aadhamad"))
+
